a2134843b0965c3f2df8ecb1e64e734434c6a86a
minrajnepali <nminraj92@gmail.com>
2023-07-04 15:33:51 -0400

    Add files via upload
    Initial Commit.

---
+++ ExcelUtility.cs
@@ -0,0 +1,367 @@
+﻿using DocumentFormat.OpenXml.Drawing.Spreadsheet;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A = DocumentFormat.OpenXml.Drawing;
+using Xdr = DocumentFormat.OpenXml.Drawing.Spreadsheet;
+using DocumentFormat.OpenXml.Drawing;
+using System.Drawing.Imaging;
+using DocumentFormat.OpenXml.Presentation;
+using System.Drawing.Drawing2D;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace GenerateTPSReport
+{
+
+    class ImageUtility
+    {
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+    class ExcelUtility
+    {
+
+        /// <summary>
+        /// Add an image to a specific cell of an existing excel spreadsheet/sheet
+        /// Create a new spreadsheet and add an image to a specific cell
+        /// Add an image to a specific cell of a WorksheetPart(so other operations can be performed before it's saved)
+        /// Add the image from a stream
+        /// Add the image from a file
+        /// Use different image formats
+        /// </summary>
+            public static ImagePartType GetImagePartTypeByBitmap(Bitmap image)
+            {
+                if (ImageFormat.Bmp.Equals(image.RawFormat))
+                    return ImagePartType.Bmp;
+                else if (ImageFormat.Gif.Equals(image.RawFormat))
+                    return ImagePartType.Gif;
+                else if (ImageFormat.Png.Equals(image.RawFormat))
+                    return ImagePartType.Png;
+                else if (ImageFormat.Tiff.Equals(image.RawFormat))
+                    return ImagePartType.Tiff;
+                else if (ImageFormat.Icon.Equals(image.RawFormat))
+                    return ImagePartType.Icon;
+                else if (ImageFormat.Jpeg.Equals(image.RawFormat))
+                    return ImagePartType.Jpeg;
+                else if (ImageFormat.Emf.Equals(image.RawFormat))
+                    return ImagePartType.Emf;
+                else if (ImageFormat.Wmf.Equals(image.RawFormat))
+                    return ImagePartType.Wmf;
+                else
+                    throw new Exception("Image type could not be determined.");
+            }
+
+            public static WorksheetPart GetWorksheetPartByName(SpreadsheetDocument document, string sheetName)
+            {
+                IEnumerable<Sheet> sheets =
+                   document.WorkbookPart.Workbook.GetFirstChild<Sheets>().
+                   Elements<Sheet>().Where(s => s.Name.ToString().Contains(sheetName));
+
+                if (sheets.Count() == 0)
+                {
+                    // The specified worksheet does not exist
+                    return null;
+                }
+
+                string relationshipId = sheets.First().Id.Value;
+                return (WorksheetPart)document.WorkbookPart.GetPartById(relationshipId);
+            }
+
+            public static void AddImage(bool createFile, string excelFile, string sheetName,
+                                        string imageFileName, string imgDesc,
+                                        int colNumber, int rowNumber)
+            {
+                using (var imageStream = new FileStream(imageFileName, FileMode.Open))
+                {
+                    AddImage(createFile, excelFile, sheetName, imageStream, imgDesc, colNumber, rowNumber);
+                }
+            }
+
+            public static void AddImage(WorksheetPart worksheetPart,
+                                        string imageFileName, string imgDesc,
+                                        int colNumber, int rowNumber)
+            {
+                using (var imageStream = new FileStream(imageFileName, FileMode.Open))
+                {
+                AddImage(worksheetPart, imageStream, imgDesc, colNumber, rowNumber);
+                }
+            }
+
+            public static void AddImage(bool createFile, string excelFile, string sheetName,
+                                        Stream imageStream, string imgDesc,
+                                        int colNumber, int rowNumber)
+            {
+                SpreadsheetDocument spreadsheetDocument = null;
+                WorksheetPart worksheetPart = null;
+                if (createFile)
+                {
+                    // Create a spreadsheet document by supplying the filepath
+                    spreadsheetDocument = SpreadsheetDocument.Create(excelFile, SpreadsheetDocumentType.Workbook);
+
+                    // Add a WorkbookPart to the document
+                    WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+                    workbookpart.Workbook = new Workbook();
+
+                    // Add a WorksheetPart to the WorkbookPart
+                    worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+                    worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+                    // Add Sheets to the Workbook
+                    Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                        AppendChild<Sheets>(new Sheets());
+
+                    // Append a new worksheet and associate it with the workbook
+                    Sheet sheet = new Sheet()
+                    {
+                        Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                        SheetId = 1,
+                        Name = sheetName
+                    };
+                    sheets.Append(sheet);
+                }
+                else
+                {
+                    // Open spreadsheet
+                    spreadsheetDocument = SpreadsheetDocument.Open(excelFile, true);
+
+                    // Get WorksheetPart
+                    worksheetPart = GetWorksheetPartByName(spreadsheetDocument, sheetName);
+                }
+
+                AddImage(worksheetPart, imageStream, imgDesc, colNumber, rowNumber);
+
+                worksheetPart.Worksheet.Save();
+
+                spreadsheetDocument.Close();
+            }
+
+            public static void AddImage(WorksheetPart worksheetPart,
+                                        Stream imageStream, string imgDesc,
+                                        int colNumber, int rowNumber)
+            {
+                // We need the image stream more than once, thus we create a memory copy
+                MemoryStream imageMemStream = new MemoryStream();
+                imageStream.Position = 0;
+                imageStream.CopyTo(imageMemStream);
+                imageStream.Position = 0;
+
+                var drawingsPart = worksheetPart.DrawingsPart;
+                if (drawingsPart == null)
+                    drawingsPart = worksheetPart.AddNewPart<DrawingsPart>();
+
+                if (!worksheetPart.Worksheet.ChildElements.OfType<Drawing>().Any())
+                {
+                    worksheetPart.Worksheet.Append(new Drawing { Id = worksheetPart.GetIdOfPart(drawingsPart) });
+                }
+
+                if (drawingsPart.WorksheetDrawing == null)
+                {
+                    drawingsPart.WorksheetDrawing = new Xdr.WorksheetDrawing();
+                }
+
+                var worksheetDrawing = drawingsPart.WorksheetDrawing;
+
+                Bitmap bm = new Bitmap(imageMemStream);
+                var imagePart = drawingsPart.AddImagePart(GetImagePartTypeByBitmap(bm));
+                imagePart.FeedData(imageStream);
+
+                A.Extents extents = new A.Extents();
+                var extentsCx = bm.Width * (long)(914400 / bm.HorizontalResolution);
+                var extentsCy = bm.Height * (long)(914400 / bm.VerticalResolution);
+                bm.Dispose();
+
+                var colOffset = 0;
+                var rowOffset = 0;
+
+                var nvps = worksheetDrawing.Descendants<Xdr.NonVisualDrawingProperties>();
+                var nvpId = nvps.Count() > 0
+                    ? (UInt32Value)worksheetDrawing.Descendants<Xdr.NonVisualDrawingProperties>().Max(p => p.Id.Value) + 1
+                    : 1U;
+
+                var oneCellAnchor = new Xdr.OneCellAnchor(
+                    new Xdr.FromMarker
+                    {
+                        ColumnId = new Xdr.ColumnId((colNumber - 1).ToString()),
+                        RowId = new Xdr.RowId((rowNumber - 1).ToString()),
+                        ColumnOffset = new Xdr.ColumnOffset(colOffset.ToString()),
+                        RowOffset = new Xdr.RowOffset(rowOffset.ToString())
+                    },
+                    new Xdr.Extent { Cx = extentsCx, Cy = extentsCy },
+                    new Xdr.Picture(
+                        new Xdr.NonVisualPictureProperties(
+                            new Xdr.NonVisualDrawingProperties { Id = nvpId, Name = "Picture " + nvpId, Description = imgDesc },
+                            new Xdr.NonVisualPictureDrawingProperties(new A.PictureLocks { NoChangeAspect = true })
+                        ),
+                        new Xdr.BlipFill(
+                            new A.Blip { Embed = drawingsPart.GetIdOfPart(imagePart), CompressionState = A.BlipCompressionValues.Print },
+                            new A.Stretch(new A.FillRectangle())
+                        ),
+                        new Xdr.ShapeProperties(
+                            new A.Transform2D(
+                                new A.Offset { X = 0, Y = 0 },
+                                new A.Extents { Cx = extentsCx, Cy = extentsCy }
+                            ),
+                            new A.PresetGeometry { Preset = A.ShapeTypeValues.Rectangle }
+                        )
+                    ),
+                    new Xdr.ClientData()
+                );
+
+                worksheetDrawing.Append(oneCellAnchor);
+            }
+        /*
+        public static void AddImage(WorksheetPart worksheetPart, string imageFileName, int rowNumber, int colNumber, int rowOffset, int colOffset)
+        {
+            var drawingsPart = worksheetPart.AddNewPart<DrawingsPart>();
+
+            if (!worksheetPart.Worksheet.ChildElements.OfType<Drawing>().Any())
+            {
+                worksheetPart.Worksheet.Append(new Drawing { Id = worksheetPart.GetIdOfPart(drawingsPart) });
+            }
+
+            if (drawingsPart.WorksheetDrawing == null)
+            {
+                drawingsPart.WorksheetDrawing = new WorksheetDrawing();
+            }
+
+            var worksheetDrawing = drawingsPart.WorksheetDrawing;
+
+            var imagePart = drawingsPart.AddImagePart(ImagePartType.Jpeg);
+
+            using (var stream = new FileStream(imageFileName, FileMode.Open))
+            {
+                imagePart.FeedData(stream);
+            }
+
+            Bitmap bm = new Bitmap(imageFileName);
+            DocumentFormat.OpenXml.Drawing.Extents extents = new DocumentFormat.OpenXml.Drawing.Extents();
+            var extentsCx = (long)bm.Width * (long)((float)914400 / bm.HorizontalResolution);
+            var extentsCy = (long)bm.Height * (long)((float)914400 / bm.VerticalResolution);
+            bm.Dispose();
+
+            // var colOffset = 0;
+            //  var rowOffset = 0;
+            // int colNumber = 5;
+            // int rowNumber = 10;
+
+            var nvps = worksheetDrawing.Descendants<Xdr.NonVisualDrawingProperties>();
+            var nvpId = nvps.Count() > 0 ?
+                (UInt32Value)worksheetDrawing.Descendants<Xdr.NonVisualDrawingProperties>().Max(p => p.Id.Value) + 1 :
+                1U;
+
+            var oneCellAnchor = new Xdr.OneCellAnchor(
+                new Xdr.FromMarker
+                {
+                    ColumnId = new Xdr.ColumnId((colNumber - 1).ToString()),
+                    RowId = new Xdr.RowId((rowNumber - 1).ToString()),
+                    ColumnOffset = new Xdr.ColumnOffset(colOffset.ToString()),
+                    RowOffset = new Xdr.RowOffset(rowOffset.ToString())
+                },
+                new Xdr.Extent { Cx = extentsCx, Cy = extentsCy },
+                new Xdr.Picture(
+                    new Xdr.NonVisualPictureProperties(
+                        new Xdr.NonVisualDrawingProperties { Id = nvpId, Name = "Picture " + nvpId, Description = imageFileName },
+                        new Xdr.NonVisualPictureDrawingProperties(new A.PictureLocks { NoChangeAspect = true })
+                    ),
+                    new Xdr.BlipFill(
+                        new A.Blip { Embed = drawingsPart.GetIdOfPart(imagePart), CompressionState = A.BlipCompressionValues.Print },
+                        new A.Stretch(new A.FillRectangle())
+                    ),
+                    new Xdr.ShapeProperties(
+                        new A.Transform2D(
+                            new A.Offset { X = 0, Y = 0 },
+                            new A.Extents { Cx = extentsCx, Cy = extentsCy }
+                        ),
+                        new A.PresetGeometry { Preset = A.ShapeTypeValues.Rectangle }
+                    )
+                ),
+                new Xdr.ClientData()
+            );
+
+            worksheetDrawing.Append(oneCellAnchor);
+
+           
+        }
+        public static void CreatePackage(string sFile, string imageFileName, int colNumber, int rowNumber, string SheetName, int  colOffset = 0, int rowOffset = 0)
+        {
+            try
+            {
+                // Create a spreadsheet document by supplying the filepath. 
+                SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.
+                    Create(sFile, SpreadsheetDocumentType.Workbook);
+
+                // Add a WorkbookPart to the document. 
+                WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+                workbookpart.Workbook = new Workbook();
+
+                // Add a WorksheetPart to the WorkbookPart. 
+                WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+                // Add Sheets to the Workbook. 
+                Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                    AppendChild<Sheets>(new Sheets());
+
+                // Append a new worksheet and associate it with the workbook. 
+                Sheet sheet = new Sheet()
+                {
+                    Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                    SheetId = 1,
+                    Name = SheetName
+                };
+                sheets.Append(sheet);
+
+
+                //AddImage(worksheetPart, imageFileName, rowNumber, colNumber, rowOffset, colOffset);
+                AddImage(worksheetPart, imageFileName, rowNumber, colNumber + 8, rowOffset, colOffset);
+               // AddImage(worksheetPart, imageFileName, 6, rowNumber, rowOffset, colOffset);
+
+                workbookpart.Workbook.Save();
+
+                // Close the document. 
+                spreadsheetDocument.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }*/
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using A = DocumentFormat.OpenXml.Drawing;
+using Xdr = DocumentFormat.OpenXml.Drawing.Spreadsheet;
+using DocumentFormat.OpenXml.Vml;
+using FilePath = System.IO.Path;
+using static System.Net.Mime.MediaTypeNames;
+using Path = System.IO.Path;
+using Image = System.Drawing.Image;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using System.Drawing.Drawing2D;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace GenerateTPSReport
+{
+    public partial class Form1 : Form
+    {
+
+        private string[] files = null;
+        private string basedir = null;
+        
+
+        static string filePath = @"C:\Users\C74340\OneDrive - Microchip Technology Inc\Desktop\Final Report\DPT_TPS.xlsx";
+
+        List<string> fileNameList = new List<string>();
+        List<string> DPTImageFileList = new List<string>();
+        List<string> FuncImageList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// On Select Folder Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Select_Folder_Click(object sender, EventArgs e)
+        {
+
+            // get the selected folder
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    basedir = fbd.SelectedPath;
+                    files = Directory.GetFiles(fbd.SelectedPath);
+                }
+            }
+
+
+            // grab all the filenames and add it to the list
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string newname = FilePath.GetFileName(files[i]).Split(new char[] {'.'})[0];
+                if (!newname.Contains("CH1") && !newname.Contains("CH2"))
+                {
+                    // check if the filename is repeated, if repeated do not add to the list.
+                    if (!sb.ToString().Contains(newname))
+                    {
+                        sb.AppendLine(newname);
+                    }
+                }
+            }
+
+
+            fileNameList = sb.ToString().Split(new char[] { '\n' }).ToList();
+
+            
+           // File.WriteAllText(Directory.GetParent(basedir) + "\\Filename.txt", sb.ToString());
+            sb.Clear();
+
+        }
+
+
+        /// <summary>
+        /// Clone a given sheet into a new sheet
+        /// </summary>
+        static void CloneSheet(string clonedSheetName)
+        {
+            using (SpreadsheetDocument spreadSheet = SpreadsheetDocument.Open(filePath, true))
+            { 
+                WorkbookPart workbookPart = spreadSheet.WorkbookPart;
+                WorksheetPart sourceSheetPart = GetWorkSheetPart(workbookPart, "template");
+                Sheets sheets = workbookPart.Workbook.GetFirstChild<Sheets>();
+
+                SpreadsheetDocument tempSheet = SpreadsheetDocument.Create(new MemoryStream(), spreadSheet.DocumentType);
+                WorkbookPart tempWorkbookPart = tempSheet.AddWorkbookPart();
+                WorksheetPart tempWorksheetPart = tempWorkbookPart.AddPart(sourceSheetPart);
+                WorksheetPart clonedSheet = workbookPart.AddPart(tempWorksheetPart);
+
+                Sheet copiedSheet = new Sheet();
+                copiedSheet.Name = clonedSheetName;
+                copiedSheet.Id = workbookPart.GetIdOfPart(clonedSheet);
+                copiedSheet.SheetId = (uint)sheets.ChildElements.Count + 1;
+                sheets.Append(copiedSheet);
+            }
+        }
+
+        /// <summary>
+        /// Get Part Id
+        /// </summary>
+        /// <param name="workbookPart"></param>
+        /// <param name="sheetName"></param>
+        /// <returns></returns>
+        static WorksheetPart GetWorkSheetPart(WorkbookPart workbookPart, string sheetName)
+        { 
+            //Get the relationship id of the sheetname
+            string relId = workbookPart.Workbook.Descendants<Sheet>() .Where(s => s.Name.Value.Equals(sheetName)) .First() .Id;
+            return (WorksheetPart)workbookPart.GetPartById(relId); 
+        }
+
+
+        /// <summary>
+        /// Generates Report for TPS
+        /// </summary>
+        private void GenerateExcelFile(string WorkSheetName, string[] ImageFilePath)
+        {
+
+            // Open the document and read all the lines into array of strings
+
+
+            string SheetName = WorkSheetName.Trim('\r');
+
+            // Add multiple images or apply further changes
+            try
+            {
+                // Open spreadsheet
+               SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(filePath, true);
+
+                // Get WorksheetPart
+                WorksheetPart worksheetPart = ExcelUtility.GetWorksheetPartByName(spreadsheetDocument, SheetName);
+
+                // Add DPT image
+                ExcelUtility.AddImage(worksheetPart, ImageFilePath[0], "DPT Image", 2, 7);  //B7
+
+
+                // Add Functional Test Image
+
+                ExcelUtility.AddImage(worksheetPart, ImageFilePath[1], "Func Image", 22, 7);
+                ExcelUtility.AddImage(worksheetPart, ImageFilePath[2], "Func Image CH1", 29, 7);
+                ExcelUtility.AddImage(worksheetPart, ImageFilePath[3], "Func Image CH2", 26, 25);
+
+                // Other operations if needed
+
+                worksheetPart.Worksheet.Save();
+
+                spreadsheetDocument.Dispose();
+            }
+            catch (Exception ex) { }
+        }
+
+
+        /// <summary>
+        /// Get all the DPT image files into a list for further use
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectDPTImageFileFolder_Click(object sender, EventArgs e)
+        {
+            // get the selected folder
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    DPTImageFileList = Directory.GetFiles(fbd.SelectedPath).ToList();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Get all the Func image files into a list for further use
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectFuncImageFolder_Click(object sender, EventArgs e)
+        {
+            // get the selected folder
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    FuncImageList = Directory.GetFiles(fbd.SelectedPath).ToList();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GenerateReport_Click(object sender, EventArgs e)
+        {
+
+
+            // Add Sheets to the existing Excel File
+
+            for (int i = 0; i < fileNameList.Count; ++i)
+            {
+                string filename = fileNameList[i].Trim('\r');
+
+                if (!string.IsNullOrEmpty(filename))
+                {
+                    CloneSheet(filename);
+                }
+            }
+
+                for (int i = 0; i < fileNameList.Count; ++i)
+            {
+                int index = i;
+
+                string[] ImageFilePath = new string[4];
+
+                // Add DPT image
+                ImageFilePath[0] = DPTImageFileList[index];
+
+                // Add Func Image
+                ImageFilePath[1] = FuncImageList[index++];
+                ImageFilePath[2] = FuncImageList[index++];
+                ImageFilePath[3] = FuncImageList[index];
+
+                string filename = fileNameList[i].Trim('\r');
+
+                if (!string.IsNullOrEmpty(filename))
+                {
+                    GenerateExcelFile(filename, ImageFilePath);
+                }
+
+            }
+            
+        }
+
+
+        /// <summary>
+        /// Resize Images
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ResizeImage_Click(object sender, EventArgs e)
+        {
+            string filepath = "C:\\Users\\C74340\\OneDrive - Microchip Technology Inc\\Desktop\\Final Report\\DPT Edited";
+            int width = 500;
+            int height = 340;
+            for (int i = 0; i < DPTImageFileList.Count; i++)
+            {
+                ImageUtility.ResizeImage(Image.FromFile(DPTImageFileList[i]), width, height).Save(Path.Combine(filepath, FilePath.GetFileName(DPTImageFileList[i])), System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+    }
+
+}
